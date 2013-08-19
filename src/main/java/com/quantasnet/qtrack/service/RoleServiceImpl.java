62a3e7e9c2632fa52902b05374091939f0d89bef
Andrew Landsverk <dewdew@gmail.com>
2013-08-18 19:29:18 -0500

    add missing @Transactional annotations

--- src/main/java/com/quantasnet/qtrack/service/RoleServiceImpl.java
+++ src/main/java/com/quantasnet/qtrack/service/RoleServiceImpl.java
@@ -3,11 +3,13 @@ package com.quantasnet.qtrack.service;
 import com.quantasnet.qtrack.domain.db.Role;
 import com.quantasnet.qtrack.domain.repo.RoleRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
+@Transactional
 public class RoleServiceImpl implements RoleService
 {
     @Resource
--- src/main/java/com/quantasnet/qtrack/service/UserServiceImpl.java
+++ src/main/java/com/quantasnet/qtrack/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.quantasnet.qtrack.service;
 import com.quantasnet.qtrack.domain.db.User;
 import com.quantasnet.qtrack.domain.repo.UserRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService
 {
     @Resource
